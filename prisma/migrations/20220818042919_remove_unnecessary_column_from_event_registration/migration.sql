5255c887b443085ba359f6b1d5966a6bfa51b664
Sankalp Mukim <sankalpmukim@gmail.com>
2022-08-18 10:04:37 +0530

    rem unnecessary column from events registration

---
+++ prisma/migrations/20220818042919_remove_unnecessary_column_from_event_registration/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `numSeatsLeft` on the `EventRegistration` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "EventRegistration" DROP COLUMN "numSeatsLeft";
