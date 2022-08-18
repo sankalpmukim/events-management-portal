eb9f5fb377b5e2ffb93a0edf24f439066551dc0e
Sankalp Mukim <sankalpmukim@gmail.com>
2022-08-18 13:45:40 +0530

    Layout component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,30 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { NextPage } from "next";
 import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "../components/Layout";
 
 export const Home: NextPage = () => {
   const { data: session } = useSession();
 
   if (session) {
     return (
-      <div className="container">
-        Welcome user {session!.user!.name!}!
-        <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </div>
+      <Layout pageTitle="Events" session={session}>
+        <div className="container">
+          Welcome user {session!.user!.name!}!
+          <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </div>
+      </Layout>
     );
   }
 
   return (
-    <div className="container">
-      Click to sign into your user account <br />
-      <button onClick={() => signIn()}>Sign in</button>
-    </div>
+    <Layout pageTitle="Events" session={session}>
+      <div className="container">
+        Click to sign into your user account <br />
+        <button onClick={() => signIn()}>Sign in</button>
+      </div>
+    </Layout>
   );
 };
 
